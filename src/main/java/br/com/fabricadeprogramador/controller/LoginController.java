c9e3e60ff5fa36f914e9405231f0a8e9bd7d3539
virmerson <virmerson@hightechcursos.com.br>
2015-12-19 17:40:47 -0300

    Iniciando com Autenticacao 3

--- src/main/java/br/com/fabricadeprogramador/controller/LoginController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/LoginController.java
@@ -3,13 +3,14 @@ package br.com.fabricadeprogramador.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.fabricadeprogramador.entidades.Usuario;
 import br.com.fabricadeprogramador.persistencia.dao.UsuarioDAO;
-
+@WebServlet(urlPatterns="/login")
 public class LoginController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -22,12 +23,19 @@ public class LoginController extends HttpServlet {
 		//2 - Colocar em um objeto do tipo usuário
 		Usuario usuario = new Usuario();
 		usuario.setLogin(login);
+		usuario.setSenha(senha);
 		
 		//3 - Busca o usuario no banco de dados
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		Usuario usuarioEncontrado = usuarioDAO.buscarPorLogin(usuario);
 		//4 - Se tiver o usuário compara a senha
-		
+		if (usuarioEncontrado.getSenha().equals(usuario.getSenha())){
+			
+			req.getRequestDispatcher("bemvindo.jsp").forward(req, resp);
+		}else {
+			
+			resp.sendRedirect("login.html");
+		}
 		//5 - Decide o redirecionamento (Login ou Bem vindo)
 		
 	}
